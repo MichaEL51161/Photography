92f8aa3c98fab7f2545c0a99e9c59cf7ff1302a0
MichaEL51161 <you@akintoyeoluwasegun001@gmail.com>
2024-04-17 10:19:15 +0100

    mine commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function myFuncOpen() {
     document.getElementById('bugger').style.display='flex';
-    document.getElementById('open').style.display='none';
+    document.getElementById('Open').style.display='none';
 }
 
 function myFuncClose() {
     document.getElementById('bugger').style.display='none';
-    document.getElementById('open').style.display='flex';
+    document.getElementById('Open').style.display='flex';
 }
